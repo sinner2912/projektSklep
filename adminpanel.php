33a2a75838d33a85313d92ef2cecba396c1c9593
Rafał Stankiewicz <rstankiewicz2912@gmail.com>
2024-07-25 21:46:29 +0200

    basic start

---
+++ adminpanel.php
@@ -0,0 +1,11 @@
+<!DOCKTYPE html>
+<html lang="en-US">
+    <head>
+
+    </head>
+    <body>
+        username:<input type="text" placeholder="username"/>
+        password:<input type="text" placeholder="password"/>
+        <input type="button" value="confirm">
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,9 @@
+<!DOCKTYPE html>
+<html lang="en-US">
+    <head>
+
+    </head>
+    <body>
+        test
+    </body>
+</html>
