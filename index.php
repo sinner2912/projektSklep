55844e5638a26b78f05566ce4b2e0dfe69da0eb2
Rafał Stankiewicz <rstankiewicz2912@gmail.com>
2024-07-28 23:11:44 +0200

    a

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 <html lang="en-US">
     <head>
         <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sklep</title>
     </head>
     <body>
         <nav>
-            <a>logo</a>
-            <a>1</a>
-            <a>2</a>
-            <a>3</a>
+            <ul>
+                <a class="left">logo</a>
+                <a class="right">1</a>
+                <a class="right">2</a>
+                <a class="right">3</a>
+            </ul>
         </nav>
         
         <main>
