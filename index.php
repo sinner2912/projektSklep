6644f952ac7b9cda4c711e4d3a2ed4d16b24f8bc
Rafał Stankiewicz <rstankiewicz2912@gmail.com>
2024-08-07 23:08:09 +0200

    g

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <!DOCKTYPE html>
 <html lang="en-US">
     <head>
-        <link rel="stylesheet" href="style.css">
+<!--        <link rel="stylesheet" href="style.css">-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sklep</title>
     </head>
     <body>
-        <nav>
-            <ul>
-                <a class="left">logo</a>
-                <a class="right">1</a>
-                <a class="right">2</a>
-                <a class="right">3</a>
-            </ul>
-        </nav>
+        <div id="nav">
+            <div id="nav_logo"><img alt="logo"></div>
+            <div class="nav_options"><p>1</p></div>
+            <div class="nav_options"><p>2</p></div>
+            <div class="nav_options"><p>3</p></div>
+        </div>
         
         <main>
         </main>
@@ -22,3 +20,53 @@
         </footer>
     </body>
 </html>
+
+
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+    }
+
+    #nav{
+        height: 4em;
+        width: 100%;
+        background-color: blueviolet;
+        display: inline-block;
+    }   
+    
+    #nav div{
+        display: inline-block;
+        background-color: aliceblue;
+        height: 100%;
+        width: 9em;
+        text-align: center;
+        
+    }
+    #nav div p{
+        vertical-align: middle;
+    }
+    
+    #nav div img{
+        vertical-align: middle;
+    }
+    
+    #nav_logo{
+        margin-left: 1em;
+        margin-right: 77em;
+    }
+
+    .nav_options{
+        
+    }
+
+    main{
+        height: 200px;
+        background-color: coral;
+    }
+
+    footer{
+        height: 200px;
+        background-color: aqua;
+    }
+</style>
